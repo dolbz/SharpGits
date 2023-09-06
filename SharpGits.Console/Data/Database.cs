c3d6e4d7635175f7c6376ade07b4079205755528
Nathan Randle <nrandle@dolbz.com>
2023-09-06 23:05:44 +0100

    Implement Database.StoreObject

--- SharpGits.Console/Data/Database.cs
+++ SharpGits.Console/Data/Database.cs
@@ -1,11 +1,18 @@
+using System.IO.Compression;
+using System.Security.Cryptography;
 using SharpGits.Console.GitObjects;
 
 namespace SharpGits.Console.Data;
 
 public class Database
 {
+    private readonly string databasePath;
+    private readonly IBlobSerializer blobSerializer;
+
     public Database(string workspacePath, IBlobSerializer blobSerializer)
     {
+        databasePath = Path.Combine(workspacePath, ".git");
+        this.blobSerializer = blobSerializer;
     }
 
     public static void Init(string workspacePath)
@@ -31,6 +38,40 @@ public class Database
 
     public void StoreObject(GitObject obj)
     {
-        throw new NotImplementedException();
+        switch (obj)
+        {
+            case Blob blob:
+                StoreSerializedObject(blobSerializer.Serialize(blob));
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private void StoreSerializedObject(byte[] obj)
+    {
+        var objectHash = HashSerializedObject(obj);
+        var subFolder = objectHash[0].ToString("x2");
+        var fileName = string.Concat(objectHash.Skip(1).Select(x => x.ToString("x2")));
+
+        var objectPath = Path.Combine(databasePath, "objects", subFolder, fileName);
+
+        var fileInfo = new FileInfo(objectPath);
+        fileInfo.Directory.Create();
+
+        using var fileStream = File.OpenWrite(objectPath);
+        using (var compressedStream = new ZLibStream(fileStream, CompressionMode.Compress))
+        {
+            compressedStream.Write(obj, 0, obj.Length);
+        }
+
+        fileStream.Close();
+    }
+
+    private byte[] HashSerializedObject(byte[] obj)
+    {
+        var sha1Digester = SHA1.Create();
+        var hashResult = sha1Digester.ComputeHash(obj);
+        return hashResult;
     }
 }
